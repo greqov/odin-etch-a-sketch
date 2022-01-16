799c06d3de6bd3d38a2d3b99e1560393dae29c54
Ivan Greqov <idgreq@bk.ru>
2022-01-16 15:25:21 +0300

    Generate grid

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
 (function () {
   'use strict';
+
+  // TODO: get grid size in px
+  const gridSize = 480;
+  const grid = document.querySelector('.js-grid');
+
+  function generateCells(num) {
+    let cells = [];
+    const size = (gridSize / num).toFixed(2);
+
+    for (let i = 0; i < num * num; i++) {
+      const cell = document.createElement('div');
+      cell.classList.add('grid__cell');
+      cell.setAttribute('style', `width: ${size}px; height: ${size}px;`);
+      cells.push(cell);
+    }
+
+    return cells;
+  }
+
+  function renderGrid(size) {
+    const cells = generateCells(size);
+    grid.innerHTML = '';
+    grid.append(...cells);
+  }
+
+  // init actions
+  renderGrid(3);
+
+  clear3.addEventListener('click', () => renderGrid(3));
+  size4.addEventListener('click', () => renderGrid(4));
+  size6.addEventListener('click', () => renderGrid(6));
 })();
